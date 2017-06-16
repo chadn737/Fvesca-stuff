69773b48d26abdfe314f444b088b47094b687fe7
Chad <chadn737@gmail.com>
2017-06-15 20:22:59 -0400

    cleaned up some stuff

--- scripts/methylC_analysis.R
+++ scripts/methylC_analysis.R
@@ -24,105 +24,11 @@ plot_features <- function(df){
          geom_vline(xintercept=40, linetype="longdash", color="grey55")
 }
 
-#define plotting function for gene-mC correlations
-plot_correlations <- function(df,mC,mC_color){
-  require(ggplot2)
-    ggplot(df, aes(genes, mC)) + geom_point(color=mC_color) +
-            theme(panel.background=element_blank(), panel.grid=element_blank(),
-            axis.text.y=element_text(color="black"), axis.text.x=element_text(color="black"),
-            axis.ticks=element_line(color="black"), axis.title=element_text(color="black"),
-            legend.position="none", axis.line=element_line(color="black")) +
-            ylab("CG methylation level") + xlab("Number of genes") +
-            scale_x_continuous(expand=c(0,0)) +
-            stat_smooth(aes(genes,mC), color="black", method="lm", se=F)
-}
-
-#Total methylation
-if(file.exists("results/total_weighted_mC.tsv")){
-  print("Plotting genome-wide total methylation")
-  df <- read.table("results/total_weighted_mC.tsv",header=T,sep="\t")
-  plot <- ggplot(df[1:3,],aes(x=Context,y=Weighted_mC,fill=Context)) +
-          geom_bar(stat = "identity") +
-          theme(panel.background=element_blank(), panel.grid=element_blank(),
-          axis.text.y=element_text(color="black"), axis.text.x=element_text(color="black"),
-          axis.ticks=element_line(color="black"), axis.title=element_text(color="black"),
-          legend.position="none", axis.line=element_line(color="black")) +
-          ylab("Percent methylation") + xlab( "" ) +
-          scale_y_continuous(limits=c(0,1), expand=c(0,0),
-          breaks=c(0.25,0.5,0.75,1),labels=c("25%","50%","75%","100%")) +
-          scale_fill_manual("",values=c("dodgerblue4","olivedrab","hotpink4"))
-  filename=paste("figures_tables/Fvesca_", args[1], "_total_methylation.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=3, useDingbats=F)
-}
-
-#Per-site methylation
-filename=paste("results/", args[1], "_per_site_boxplot.tsv", sep="")
-if(!file.exists(filename)){
-  print("Combining per-site methylation data")
-  x <- data.frame()
-  context <- c('CG','CGA','CGT','CGC','CGG','CHG','CAG','CTG','CCG','CHH',
-               'CAA','CAT','CAC','CTA','CTT','CTC','CCA','CCT','CCC')
-  for(seq in context){
-      input=paste("results/", seq, "_site_methylation.txt", sep="")
-      df <- read.table(input,header=T)
-      x <- rbind(x,cbind(rbind(boxplot.stats(df$mc_level)$stats),rbind(boxplot.stats(df$mc_level)$conf)))
-      file.remove(input)
-  }
-  row.names(x) <- context
-  colnames(x) <- c('ymin','lower','middle','upper','ymax','notchlower','notchupper')
-  write.table(x,filename,quote=F,sep='\t')
-}
-
-if(file.exists(filename)){
-  print("Making per-site methylation plot")
-  x <- read.table(filename,header=T,sep="\t")
-  x$order <- c(1:19)
-  x$color <- c(1,1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,3,3,3)
-  plot <- ggplot(x, aes(reorder(row.names(x),order), fill=factor(color))) +
-          geom_boxplot(aes(ymin=ymin,lower=lower,middle=middle,upper=upper,ymax=ymax,
-            notchlower=notchlower,notchupper=notchupper),stat="identity",notch=T) +
-            theme(panel.background=element_blank(), panel.grid=element_blank(),
-            axis.text.y=element_text(color="black"), axis.text.x=element_text(color="black"),
-            axis.ticks=element_line(color="black"), axis.title=element_text(color="black"),
-            legend.position="none", axis.line=element_line(color="black")) +
-            ylab("Methylation level") + xlab("Context") +
-            scale_y_continuous(limits=c(0,1.05), expand=c(0,0),
-            breaks=c(0.25,0.5,0.75,1),labels=c("25%","50%","75%","100%")) +
-            scale_fill_manual("",values = c("dodgerblue4","olivedrab","hotpink4"))
-  filename=paste("figures_tables/Fvesca_", args[1], "_per_site_boxplot.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=8, useDingbats=F)
-}
-
-#genome distributions
+#plot chr metaplots
 if(file.exists("results/genome_windows_data.tsv")){
   print("Making genome distribution plots")
   df <- read.table("results/genome_windows_data.tsv",header=T,sep="\t")
 
-  #plot mCG gene correlations
-  plot <- plot_correlations(df,df$mCG,"dodgerblue4") +
-          scale_y_continuous(limits=c(0,1), expand=c(0,0),
-          breaks=c(0.25,0.5,0.75,1),labels=c("25%","50%","75%","100%"))
-  filename=paste("figures_tables/Fvesca_", args[1], "_gene_mCG_correlation.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=4, useDingbats=F)
-  rm(plot)
-
-  #plot mCHG gene correlations
-  plot <- plot_correlations(df,df$mCHG,"olivedrab") +
-          scale_y_continuous(limits=c(0,1), expand=c(0,0),
-          breaks=c(0.25,0.5,0.75,1),labels=c("25%","50%","75%","100%"))
-  filename=paste("figures_tables/Fvesca_", args[1], "_gene_mCHG_correlation.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=4, useDingbats=F)
-  rm(plot)
-
-  #plot mCHH gene correlations
-  plot <- plot_correlations(df,df$mCHH,"hotpink4") +
-          scale_y_continuous(limits=c(0,0.1), expand=c(0,0),
-          breaks=c(0.025,0.05,0.075,0.1),labels=c("2.5%","5.0%","7.5%","10%"))
-  filename=paste("figures_tables/Fvesca_", args[1], "_gene_mCHH_correlation.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=4, useDingbats=F)
-  rm(plot)
-
-  #plot chr metaplots
   print("Making chromosome plots")
   library(stringi)
   df$chr <- stri_replace_last(df$window,'',regex="_[0-9]+.*?")
@@ -151,17 +57,6 @@ if(file.exists("results/genome_windows_data.tsv")){
      }
   }
   rm(df)
-}
-
-#plot repeat metaplots
-if(file.exists("results/repeat_metaplot.tsv")){
-  print("Making repeat metaplot")
-  df <- read.table("results/repeat_metaplot.tsv",header=T,sep="\t")
-  plot <- plot_features(df) +
-          scale_x_continuous(labels=c("-2000","5'","3'","+2000"), breaks=c(1, 20, 40, 60))
-  filename=paste("figures_tables/Fvesca_", args[1], "_repeat_metaplot.pdf", sep="")
-  ggsave(filename=filename, plot, height=4, width=4, useDingbats=F)
-  rm(df,plot)
 }
 
 #plot gene metaplots
--- scripts/methylC_analysis.py
+++ scripts/methylC_analysis.py
@@ -12,21 +12,9 @@ allc="allc/Fvesca_"+sys.argv[1]+"_allc_total.tsv"
 fasta="../ref/"+sys.argv[1]+"/bowtie2/Fvesca_"+sys.argv[1]+".fa"
 genome_file="../ref/"+sys.argv[1]+"/misc/Fvesca_"+sys.argv[1]+".genome"
 genes_gff="../ref/"+sys.argv[1]+"/misc/Fvesca_"+sys.argv[1]+".gff"
-unique_genes="../ref/"+sys.argv[1]+"/misc/Fvesca_"+sys.argv[1]+"_unique.gff"
-syntenic_genes="../ref/"+sys.argv[1]+"/misc/Fvesca_"+sys.argv[1]+"_syntelogs.gff"
-repeats_gff="../ref/"+sys.argv[1]+"/misc/Fvesca_"+sys.argv[1]+"_filtered_repeats.gff"
 filter_chr=['ChrL','ChrC','ChrM']
 context=['CG','CHG','CHH','CAA','CAT','CAC','CAG','CTA','CTT','CTC',
          'CTG','CCA','CCT','CCC','CCG','CGA','CGT','CGC','CGG']
-
-print("Finding Total Weighted Methylation")
-functions.weighted_mC(allc,output="results/total_weighted_mC.tsv",cutoff=0)
-
-print("Getting per-site methylation levels")
-functions.per_site_mC(allc,output_path='results/',context=context)
-
-print("Analyzing Subcontext Methylation")
-functions.subcontext_methylation(allc,fasta,context=context,output='results/subcontext_methylation.tsv',filter_chr=filter_chr)
 
 print("Finding mC distributions")
 functions.mC_distribution(genome_file,allc,genes_gff,fasta,
@@ -39,26 +27,5 @@ if os.path.exists(genes_gff):
                             ignoreStrand=False,windows=60,updown_stream=2000,cutoff=0,
                             first_feature='mRNA',second_feature='CDS',filter_chr=filter_chr,
                             remove_tmp=False)
-else:
-    print("No gene annotations found")
-
-if os.path.exists(syntenic_genes):
-    print("Syntenic genes metaplot")
-    functions.feature_metaplot(allc,syntenic_genes,genome_file,output="results/syntenic_gene_metaplot.tsv",
-                               ignoreStrand=False,windows=60,updown_stream=2000,cutoff=0)
-else:
-    print("No syntentic genes found")
-
-if os.path.exists(unique_genes):
-    print("Unique genes metaplot")
-    functions.feature_metaplot(allc,unique_genes,genome_file,output="results/unique_gene_metaplot.tsv",
-                               ignoreStrand=False,windows=60,updown_stream=2000,cutoff=0)
-else:
-    print("No unique genes found")
-
-if os.path.exists(genes_gff):
-    print("Gene methylation levels")
-    functions.feature_mC_levels('CDS_allc.tmp',genes_gff,output="results/gene_methylation_levels.tsv",
-                                cutoff=0,filter_features='gene',filter_chr=filter_chr)
 else:
     print("No gene annotations found")
